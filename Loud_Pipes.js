40db186247ba1a21ef2842d379073583762ffe2a
tkapa <1005732@Student.sae.edu.au>
2017-03-10 02:15:58 +1000

    Find card active
    Loud Pipes can now scan the database for a specific card. This syntax must be exact. Returns the same embed as the random function.

--- Loud_Pipes.js
+++ Loud_Pipes.js
@@ -15,85 +15,121 @@ fs.readFile(`${__dirname}/cardList.json`, (err, data) => {
 
 client.connect({ token: "Mjg2NDI3NTk2MDI2MDE5ODQx.C5gmyA.EWk6RRGeTbV6KHSdc-7-y5DXov8" });
 
-client.Dispatcher.on("GATEWAY_READY", e => {
-  console.log("Connected as: " + client.User.username);
-  client.Channels.get('216455483094073344').sendMessage('Suh dude, I\'m back again boiz');
+    client.Dispatcher.on("GATEWAY_READY", e => {
+        client.Channels.get('216455483094073344').sendMessage('***CLANK***');
+        client.Channels.get('216455483094073344').sendMessage(`you called?`);
+        client.User.setGame('r for card');
 });
 
 client.Dispatcher.on("MESSAGE_CREATE", sentMessage => {
-  if (sentMessage.message.content == "!randCard") RandomCard(sentMessage);
+    if (sentMessage.message.content == "Yo pipes")
+        sentMessage.message.channel.sendMessage(`Hello, ${sentMessage.message.author}`);
+    if (sentMessage.message.content == "r") RandomCard(sentMessage);
+    if(sentMessage.message.content.substring(0,5) == '!find') FindCard(sentMessage);
 
-
-  if (sentMessage.message.content == "!downloadCardSets")  CompileCards(sentMessage);
-  if (sentMessage.message.content == "!saveCardSets")  GenJSON(sentMessage);
+    //Should only be used if the data is needed again
+    if (sentMessage.message.content == "!downloadCardSets")  CompileCards(sentMessage);
+    if (sentMessage.message.content == "!saveCardSets")  GenJSON(sentMessage);
 });
 
+//Generates a random card from the database
 function RandomCard(sentMessage){
   console.log(`We have ${globdata.length} cards`)
   var cardNo = Math.round(Math.random() * globdata.length);
   console.log(`CardNo is ` + cardNo);
   var thisCard = globdata[cardNo];
 
-  //Preemptively create the cardEmbed
-  //https://leovoel.github.io/embed-visualizer/ card embed sim
-  var cardEmbed = {
-      author: {
-          name: `A card has been retrieved`
-      },
-      title: `${thisCard.name} ${thisCard.manaCost}`,
-      description: `*${thisCard.flavor}*`,
-      timestamp: new Date(),
-      image: {
-          url: `${thisCard.imageUrl}`
-      },
-      color: 0xd3d3d3,
-      footer: {
-          text: `Card art by ${thisCard.artist}`
-      },
-      fields: [
-          {
-              name: `${thisCard.type}`,
-              value: `${thisCard.originalText}`
-          },
-          {
-              name: `Set: ${thisCard.setName}`,
-              value: `??`
-          }
-      ]
-      }
+  embedCardData(sentMessage, thisCard);
+}
 
-      //Change the color of the embed depending on the card color. the default card color is grey
-      if(thisCard.colors) if(thisCard.colors.length > 0){
-          switch (thisCard.colors[0]) {
-            case 'Blue':
-                    cardEmbed.color = 0xadd8e6;
-                break;
+//Search for a specific cardEmbed
+function FindCard(sentMessage){
+    var foundCard;
+    //var messageArr = sentMessage.message.content.split('_');
+    var messageArr = sentMessage.message.content.slice(6, sentMessage.message.content.length);
+    console.log(messageArr);
 
-            case 'Green':
-                    cardEmbed.color = 0x00ff00;
-                break;
+    for(i = 0; i < globdata.length - 1; ++i){
+        if(globdata[i].name == messageArr){
+            foundCard = globdata[i];
+            break;
+        }
+    }
 
-            case 'Red':
-                    cardEmbed.color = 0xff0000;
-                break;
+    if(foundCard)
+        embedSingleCardData(sentMessage, foundCard);
+    else {
+        sentMessage.message.channel.sendMessage(`Sorry, ${sentMessage.message.author.mention}, I couldn't find that card`);
+    }
+}
 
-            case 'Black':
-                    cardEmbed.color = 0x000000;
-                break;
+function embedSingleCardData(sentMessage, thisCard){
+    //Preemptively create the cardEmbed
+    //https://leovoel.github.io/embed-visualizer/ card embed sim
+    var cardEmbed = {
+        author: {
+            name: `A card has been retrieved`
+        },
+        title: `${thisCard.name} ${thisCard.manaCost}`,
+        description: `*No flavour text available*`,
+        timestamp: new Date(),
+        image: {
+            url: `${thisCard.imageUrl}`
+        },
+        color: 0xd3d3d3,
+        footer: {
+            text: `Card art by ${thisCard.artist}`
+        },
+        fields: [
+            {
+                name: `${thisCard.type}`,
+                value: `??`,
+            },
+            {
+                name: `Set:`,
+                value: `${thisCard.setName}`,
+            },
+            {
+                name: `Rarity:`,
+                value: `${thisCard.rarity}`,
+            }
+        ]
+        }
 
-            case 'White':
-                    cardEmbed.color = 0xffffff;
-                break;
+        if(thisCard.flavor) cardEmbed.description = `*${thisCard.flavor}*`;
+        if(thisCard.originalText) cardEmbed.fields[0].value = `${thisCard.originalText}`;
 
-            default:
-                    cardEmbed.color = 0xd3d3d3;
-                break;
-          }
-      }
+        //Change the color of the embed depending on the card color. the default card color is grey
+        if(thisCard.colors) if(thisCard.colors.length > 0){
+            switch (thisCard.colors[0]) {
+              case 'Blue':
+                      cardEmbed.color = 0xadd8e6;
+                  break;
 
+              case 'Green':
+                      cardEmbed.color = 0x00ff00;
+                  break;
 
-  //If the message sent is !ping send an embed containing this info
-  sentMessage.message.channel.sendMessage('', false, cardEmbed);
+              case 'Red':
+                      cardEmbed.color = 0xff0000;
+                  break;
+
+              case 'Black':
+                      cardEmbed.color = 0x000000;
+                  break;
+
+              case 'White':
+                      cardEmbed.color = 0xffffff;
+                  break;
+
+              default:
+                      cardEmbed.color = 0xd3d3d3;
+                  break;
+            }
+        }
+
+    //If the message sent is !ping send an embed containing this info
+    sentMessage.message.channel.sendMessage('', false, cardEmbed);
 }
 
 //FUNCTIONS REQUIRED FOR COMPILING MTG CARDS FROM AN EXTERNAL API
